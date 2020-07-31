873ad36f3e812c374dd9bbe1c3c986adb39c23c7
yanghuazhen <yanghuazhen@ctfo.com>
2020-07-31 16:27:53 +0800

    add Client

---
+++ template-method-patterns/src/main/java/org/example/Client.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * 模板方法模式本质：
+ *
+ */
+public class Client
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
